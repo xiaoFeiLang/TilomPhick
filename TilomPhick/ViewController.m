5475b7fe4087aadc0be50672ca8c9ffed0f012ff
loulou <627265625@qq.com>
2016-07-23 23:32:58 +0800

    .

--- TilomPhick/ViewController.m
+++ TilomPhick/ViewController.m
@@ -10,7 +10,13 @@
 #import "HomeVC.h"
 
 @interface ViewController ()
+{
 
+    NSArray *items;
+    NSMutableDictionary *stateDictionary;
+    
+    NSIndexPath *lastIndexPath;
+}
 @end
 
 @implementation ViewController
@@ -18,24 +24,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-}
+    items = [@"Lunbo Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Romeo Quebec Sierra Tango Uniform Victor Whiskey Xray Yankee Zulu" componentsSeparatedByString:@" "];
+    stateDictionary = [NSMutableDictionary dictionary];
 
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+    return items.count;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
 
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    cell.textLabel.text = @"lunbo";
+    cell.textLabel.text = items[indexPath.row];
+    
+    
+//    单选
+    NSUInteger row = [indexPath row];
+    NSUInteger oldRow = [lastIndexPath row];
+    cell.accessoryType = (row == oldRow && lastIndexPath != nil) ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    
+//    多选
+//    BOOL isChecked = ((NSNumber *)stateDictionary[indexPath]).boolValue;
+//    cell.accessoryType = isChecked ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+//    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSInteger newRow = [indexPath row];
+    NSInteger oldRow = (lastIndexPath != nil) ? [lastIndexPath row] : -1;
 
-    HomeVC *vc = [[HomeVC alloc] init];
-    [self.navigationController pushViewController:vc animated:YES];
+//    单选
+    if (newRow != oldRow) {
+        UITableViewCell *newCell = [tableView cellForRowAtIndexPath:
+                                    indexPath];
+        newCell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+        UITableViewCell *oldCell = [tableView cellForRowAtIndexPath:
+                                    lastIndexPath];
+        oldCell.accessoryType = UITableViewCellAccessoryNone;
+        lastIndexPath = indexPath;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+//    多选
+//    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+//    BOOL isChecked = !((NSNumber *)stateDictionary[indexPath]).boolValue;
+//    stateDictionary[indexPath] = @(isChecked);
+//    cell.accessoryType = isChecked ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    
+    
+//    HomeVC *vc = [[HomeVC alloc] init];
+//    [self.navigationController pushViewController:vc animated:YES];
+    
+    
 }
 @end
